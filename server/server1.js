0141e7450675eb8b46922224c36ffd0aedaf48db
dhanushpvenkitesh <dhanushpvenkitesh@gmail.com>
2024-08-29 11:21:06 +0530

    first time

--- server/server1.js
+++ server/server1.js
@@ -2,9 +2,25 @@ const http = require('http');
 const port = 3000;
 const url = require('url');
 const fs = require('fs');
-const queryString = require('querystring')
+const queryString = require('querystring');
+const {MongoClient}=require ('mongodb');
+const { error } = require('console');
+const client = new MongoClient('mongodb://localhost:27017');
+
+async function connect(){
+    try{
+    await client.connect();
+    console.log("database connection established....")
+} catch(error){
+    console("error ",error)
+
+}
+}
+connect();
 
 const server = http.createServer((req, res)=>{
+    let db=client.db("dms");
+    let collection=db.collection("users")
     const req_url = req.url;
     console.log("req_url : ",req_url);
 
@@ -46,6 +62,24 @@ const server = http.createServer((req, res)=>{
             console.log("name : ", datas.name);
             console.log("email : ", datas.email);
             console.log("password: ", datas.password);
+            collection.insertOne({
+                name : datas.name,
+                email : datas.email,
+                password :datas.password,
+
+            })
+            .then((message) =>{
+                console.log("message :",message);
+                res.writeHead(201,{'Content_type' : "text/plain"});
+                res.end("user created.....");
+
+            })
+            .catch((error)=>{
+                console.log("error",error)
+
+                res.writeHead(400,{"Content_type" :"text/plain"});
+                res.end(error.message ? error.message : "user creation failed");
+            })
         })
     }
 
